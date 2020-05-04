8d96ed65846b61813ed31c05f891942c30689abf
harimbu <harimbu@gmail.com>
2020-05-04 16:43:50 +0900

    trovo

--- js/main.js
+++ js/main.js
@@ -1,33 +1,36 @@
-const btn = document.querySelector(".menu_toggle");
+// Mavigation toggle
 const nav = document.querySelector(".nav");
+const btn = document.querySelector(".menu_toggle");
 
 btn.addEventListener("click", function () {
   nav.classList.toggle("open");
 });
 
-// Scroll Reveal
-ScrollReveal().reveal(".left_ani", {
+//Scroll Reveal
+// ScrollReveal().reveal(target, options);
+window.sr = ScrollReveal();
+sr.reveal(".left_ani", {
   origin: "left",
   duration: 1000,
   distance: "25rem",
   delay: 300,
 });
 
-ScrollReveal().reveal(".right_ani", {
+sr.reveal(".right_ani", {
   origin: "right",
   duration: 1000,
   distance: "25rem",
   delay: 600,
 });
 
-ScrollReveal().reveal(".top_ani", {
+sr.reveal(".top_ani", {
   origin: "top",
   duration: 1000,
   distance: "25rem",
   delay: 300,
 });
 
-ScrollReveal().reveal(".bottom_ani", {
+sr.reveal(".bottom_ani", {
   origin: "bottom",
   duration: 1000,
   distance: "25rem",
@@ -35,17 +38,16 @@ ScrollReveal().reveal(".bottom_ani", {
 });
 
 // Parallax
-$(".stillness").parallax({ imageSrc: "../images/slide_02.jpg" });
-$(".alone").parallax({ imageSrc: "../images/slide_03.jpg" });
+$(".stillness").parallax({ imageSrc: "./images/slide_02.jpg" });
+$(".alone").parallax({ imageSrc: "./images/slide_03.jpg" });
 
-// Slide slick
 $(".slide_area").slick({
   arrows: false,
   autoplay: true,
-  fade: true,
 });
 
 $(".slide_area2").slick({
   arrows: false,
   autoplay: true,
+  fade: true,
 });
